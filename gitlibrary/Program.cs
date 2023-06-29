7d689e66974af0b4079eee9d66f7b749a020c0f6
Anilkumar <anilkumar-ibaseit>
2023-06-29 16:42:08 +0530

    Commit changes from master to new branch

--- gitlibrary/Program.cs
+++ gitlibrary/Program.cs
@@ -46,7 +46,7 @@ namespace gitlibrary
                 var changes = repo.Diff.Compare<TreeChanges>(branch.Tip.Tree, DiffTargets.Index | DiffTargets.WorkingDirectory);
                 Commands.Stage(repo, changes.Select(c => c.Path));
                 Commit commit = repo.Commit("Commit changes from master to new branch", author, committer);
-                repo.Branches.Update(repo.Head,
+                repo.Branches.Update(branch,
                    b => b.Remote = repo.Network.Remotes["gitlibrary"].Name,
                    b => b.UpstreamBranch = repo.Head.CanonicalName);
                 var pushOptions = new PushOptions
