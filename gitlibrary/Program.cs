11b459d6918d69bfc7caf6522c4640d4d9a3f28e
Anilkumar <anilkumar-ibaseit>
2023-06-29 16:39:04 +0530

    Commit changes from master to new branch

--- gitlibrary/Program.cs
+++ gitlibrary/Program.cs
@@ -41,17 +41,18 @@ namespace gitlibrary
                 // Commit and push changes
                 var author = new Signature("Anilkumar", "anilkumar-ibaseit", DateTimeOffset.Now);
                 var committer = author;            
-                var pushOptions = new PushOptions
-                {
-                    CredentialsProvider = (_, __, ___) =>
-                        new UsernamePasswordCredentials { Username = token, Password = "" }
-                };
+               
                 // update branch references
                 repo.Branches.Update(repo.Head, updater =>
                 {
                     updater.Remote = repo.Network.Remotes["gitlibrary"].Name;
                     updater.UpstreamBranch = repo.Head.CanonicalName;
                 });
+                var pushOptions = new PushOptions
+                {
+                    CredentialsProvider = (_, __, ___) =>
+                        new UsernamePasswordCredentials { Username = token, Password = "" }
+                };
                 repo.Merge(branch, repo.Config.BuildSignature(DateTimeOffset.Now));
                 var changes = repo.Diff.Compare<TreeChanges>(branch.Tip.Tree, DiffTargets.Index | DiffTargets.WorkingDirectory);
                 Commands.Stage(repo, changes.Select(c => c.Path));
